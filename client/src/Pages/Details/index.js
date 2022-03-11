63bc822c8a0e3be40cc6e17ba2e9eef2234d9bd6
Rodrigo Lima <rodrigootavio19@gmail.com>
2022-03-10 21:34:29 -0400

    feat: addd estrutura para receber detalhes e videos

--- client/src/Pages/Details/index.js
+++ client/src/Pages/Details/index.js
@@ -1,7 +1,54 @@
-import React from 'react';
-
+import React, { useEffect, useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import AppContext from '../../context/AppContext';
+import handleLists from '../../services/handleLists';
+
 export default function Details() {
-  return <div>{console.log(useParams())}</div>;
+  const { id, type } = useParams();
+  const { appLanguage } = useContext(AppContext);
+  const [isMounted, setIsMounted] = useState(false);
+  const [videoKey, setVideoKey] = useState('');
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    async function getDetailAndVideos() {
+      const options = { id, type, language: appLanguage };
+
+      const [detailsInfo, videosInfo] = await Promise.all([
+        handleLists.getDetails(options),
+        handleLists.getVieos(options),
+      ]);
+
+      setDetails(detailsInfo);
+      console.log(detailsInfo);
+      const officialVideosInfo = videosInfo.filter((info) => info.official);
+      let firstKey;
+
+      if (officialVideosInfo.length >= 1) {
+        firstKey = officialVideosInfo[0].key;
+      }
+      setVideoKey(firstKey);
+    }
+
+    if (!isMounted) {
+      getDetailAndVideos();
+      setIsMounted(true);
+    }
+  }, [appLanguage, id, type, isMounted]);
+
+  return (
+    isMounted && (
+      <div>
+        <iframe
+          width="320"
+          height="240"
+          src={`https://www.youtube.com/embed/${videoKey}?autoplay=1&mute=1&loop=1`}
+          title={details.title}
+        >
+          Your browser does not support iframes.
+        </iframe>
+      </div>
+    )
+  );
 }
--- client/src/services/handleLists.js
+++ client/src/services/handleLists.js
@@ -3,39 +3,62 @@ import axios from 'axios';
 const { REACT_APP_KEY_MOVIEDB: KEY_MOVIEDB } = process.env;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-async function getPopular({ page, lenguage, type }) {
+async function getPopular({ page, language, type }) {
   try {
     const { data } = await axios.get(
-      `${BASE_URL}/${type}/popular?api_key=${KEY_MOVIEDB}&language=${lenguage}&page=${page}`,
+      `${BASE_URL}/${type}/popular?api_key=${KEY_MOVIEDB}&language=${language}&page=${page}`,
     );
 
     return data.results;
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
 
     return [];
   }
 }
 
-async function getTopRated({ page, lenguage, type }) {
+async function getTopRated({ page, language, type }) {
   try {
     const { data } = await axios.get(
-      `${BASE_URL}/${type}/top_rated?api_key=${KEY_MOVIEDB}&language=${lenguage}&page=${page}`,
+      `${BASE_URL}/${type}/top_rated?api_key=${KEY_MOVIEDB}&language=${language}&page=${page}`,
     );
 
     return data.results;
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
 
     return [];
   }
 }
 
-// async function getDetails({ id, lenguage, type }) {
-//   try {
-//   } catch (error) {}
-// }
+async function getDetails({ id, language, type }) {
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}/${type}/${id}?api_key=${KEY_MOVIEDB}&language=${language}`,
+    );
 
-const handleLists = { getPopular, getTopRated };
+    return data;
+  } catch (err) {
+    console.log(err.message);
+
+    return {};
+  }
+}
+
+async function getVieos({ id, type, language }) {
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}/${type}/${id}/videos?api_key=${KEY_MOVIEDB}&language=${language}`,
+    );
+
+    return data.results;
+  } catch (err) {
+    console.log(err.message);
+
+    return {};
+  }
+}
+
+const handleLists = { getPopular, getTopRated, getDetails, getVieos };
 
 export default handleLists;
